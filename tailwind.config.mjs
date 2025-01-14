cf7b5a4757e1467a9144001c61b7b2cb72215d31
Angel Gala <85372974+AngelDev2001@users.noreply.github.com>
2025-01-14 16:33:05 -0500

    Migrate project to astro (#17)
    * add: added header
    
    * add carousel section
    
    * add main section
    
    * refactoring code
    
    * Add: added content wrapper component, config prettier and refactored code
    
    * add footer
    
    * refactor: refactored main section
    
    * add plans section
    
    * refactoring-code
    
    * refactor: refactored code
    
    * add: added success page
    
    * add: added contact form component and configuration
    
    * refactored code
    
    * configuration
    
    * add: added meta tags
    
    * add: added favicon
    
    ---------
    
    Co-authored-by: Joaquin-Huaranga <joaquinhuarangaorihuela@gmail.com>

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,9 +1,33 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
-	theme: {
-		extend: {},
-	},
-	plugins: [
-	],
-}
+  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  theme: {
+    mode: "jit",
+    extend: {
+      colors: {
+        primary: "#FFF100",
+        secondary: "#0A0A0A",
+        tertiary: "#131313",
+        quaternary: "#0B0E10",
+      },
+      animation: {
+        marquee: "marquee 25s linear infinite",
+        marquee2: "marquee2 25s linear infinite",
+      },
+      keyframes: {
+        marquee: {
+          "0%": { transform: "translateX(0%)" },
+          "100%": { transform: "translateX(-100%)" },
+        },
+        marquee2: {
+          "0%": { transform: "translateX(100%)" },
+          "100%": { transform: "translateX(0%)" },
+        },
+      },
+      gridTemplateColumns: {
+        fill: "repeat(auto-fill, minmax(280px, 1fr))",
+      },
+    },
+  },
+  plugins: [],
+};
